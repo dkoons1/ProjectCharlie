8a0255504d6c379dc84704d9df2c7a22c8c72277
DStark <hindash@email.arizona.edu>
2019-11-21 21:00:07 -0700

    display first quote

--- quotes.js
+++ quotes.js
@@ -3,50 +3,36 @@
 // 0352ff7299msh43077480e8043c9p18689ejsn933c8cc390b4
 
 //
-var settings = {
-  "async": true,
-  "crossDomain": true,
-  "url": "https://type.fit/api/quotes",
-  "method": "GET"
-}
+var queryURL = "https://type.fit/api/quotes";
 
-$.ajax(settings).done(function (response)
-{
-  var data = JSON.parse(response);
-  console.log(data);
-
+$.ajax({
+  url: queryURL,
+  method: "GET",
+})
+  .then(function (response)
   {
-
-    var queryURL = "https://type.fit/api/quotes";
-
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    })
-      .then(function (response)
-      {
-        // console.log(queryURL);
-        // console.log(response);
-
-        var text = $("<p>").text(response.text);
-        var author = $("<p>").text(response.author);
-        $("#randomQuote").html(response.text.author);
-        console.log(response.text.author)
-
-        for (var i = 0; i < results.length; i++) {
+    console.log(queryURL);
+    console.log(response);
 
 
-          var randomQuote = $("<div>");
-          var q = $("<p>").text("text: " + results[i].author);
+    var parsed = JSON.parse(response);
 
-          randomQuote.attr("src", results[i].quote.text.url);
+    var text = $("<p>").text(parsed[0].text + " " + parsed[0].author);
+    var author = $("<p>").text(parsed[0].author);
+    $("#randomQuote").html(text);
+    console.log(response.text.author)
 
-          randomQuote.append(p);
-          randomQuote.append(author);
-          console.log(response.text.author);
-          $("#randomQuote").prepend(randomQuote);
-        }
-      })
-  }
+    for (var i = 0; i < parsed.length; i++) {
 
-});
+
+      var randomQuote = $("<div>");
+      var q = $("<p>").text("text: " + results[i].author);
+
+      randomQuote.attr("src", results[i].quote.text.url);
+
+      randomQuote.append(p);
+      randomQuote.append(author);
+      console.log(response.text.author);
+      $("#randomQuote").prepend(randomQuote);
+    }
+  })
